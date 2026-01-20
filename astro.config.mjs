9563712a75fa4ea6700fb8c93a73e11a52986d8a
Tonyajca21 <Castroanthony1806@gmail.com>
2026-01-20 16:33:00 -0600

    disc

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node'; // ← nuevo adaptador
 
 export default defineConfig({
-  output: 'server', // ← importante para rutas dinámicas
-adapter: node({
-  mode: 'standalone', // genera un build que se puede ejecutar solo con Node         // carpeta de salida del build
-}),
+  output: 'static', // <-- genera HTML estático
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
   }
 });
+
