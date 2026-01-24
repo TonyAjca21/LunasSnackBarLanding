ff6641310fa06cf317096480441495e97bc44e14
Tonyajca21 <Castroanthony1806@gmail.com>
2026-01-23 20:32:40 -0600

    fix:evnteid.tsx/astro

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_Odh7FBTs.mjs';
-import { manifest } from './manifest_c0xI06pT.mjs';
+import { manifest } from './manifest_A_wqV6MI.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "74705524-7372-4083-baaf-fd072b920ffe",
+    "middlewareSecret": "7e2377e9-21d6-4b76-8797-066e948d13e6",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
